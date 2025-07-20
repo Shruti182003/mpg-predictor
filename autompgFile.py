59bd0bd677578bad3dc586dcc0c47bc715ef368f
Shruti Shreya <shrutishreya5720@gmail.com>
2025-07-20 13:45:27 +0530

    Add files via upload

--- autompgFile.py
+++ autompgFile.py
@@ -8,6 +8,9 @@ from sklearn.preprocessing import StandardScaler
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
+with open("scaler.pkl", "rb") as f:
+    scaler = pickle.load(f)
+
 st.title("MPG Prediction App")
 st.write("Enter car specifications to predict MPG (Miles Per Gallon).")
 
@@ -21,11 +24,10 @@ model_year = st.slider("Model Year", 70, 82, 76)
 origin = st.selectbox("Origin", options=[1, 2, 3], format_func=lambda x: {1: "USA", 2: "Europe", 3: "Japan"}[x])
 
 # Make prediction
-sc = StandardScaler()
-a = np.array([cylinders, displacement, horsepower, weight, acceleration, model_year, origin]).reshape(1,-1)
-a = sc.fit_transform(a)
+input_data = np.array([cylinders, displacement, horsepower, weight, acceleration, model_year, origin]).reshape(1,-1)
+input_scaled = scaler.transform(input_data)
 #user_input = sc.fit(np.array([cylinders, displacement, horsepower, weight, acceleration, model_year, origin]).reshape(1,-1))
-prediction = model.predict(a)
+prediction = model.predict(input_scaled)
 
 # Display prediction
 st.subheader("Predicted MPG:")
